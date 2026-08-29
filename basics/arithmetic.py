b259a5177dd411cb9286e2025493367edc686ed0
Yatharth Mehra <mehrayatharth07@gmail.com>
2026-08-29 22:34:20 +0530

    Added arithmetic program

---
+++ basics/arithmetic.py
@@ -0,0 +1,16 @@
+num1=input("Please enter a number")   #Takes the input number from the user 
+num2=input("Please enter a number")
+num1=int(num1)   #Converts string value to integer
+num2=int(num2)  
+
+sum=num1+num2
+sub=num1-num2
+mul=num1*num2
+div=num1/num2    #divides the two numbers and returns a float value
+rem=num1%num2    #returns the remainder 
+
+print("Sum of the given numbers=",sum)       #prints the output of the expression
+print("subtraction of the given numbers=",sub)      #prints the output of the expression
+print("Multiplication of the given numbers=",mul)   #prints the output of the expression
+print("division of the given numbers=",div)     #prints the output of the expression
+print("remainder of the given numbers=",rem)    #prints the output of the expression
