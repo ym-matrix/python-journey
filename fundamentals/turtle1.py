8ae4774b84d2b9f9a1473f865b9beb491f170f6a
Yatharth Mehra <mehrayatharth07@gmail.com>
2026-08-31 22:29:33 +0530

    Added turtle1 program

---
+++ fundamentals/turtle1.py
@@ -0,0 +1,18 @@
+import turtle 
+wn = turtle.Screen()
+wn.bgcolor("black")
+alex=turtle.Turtle()
+alex.color("orange")
+alex.pensize(5)
+
+alex.forward(150)
+alex.left(90)   
+alex.forward(150)
+alex.left(45)
+alex.forward(105)
+alex.left(90)
+alex.forward(105)
+alex.left(45)
+alex.forward(150)
+
+wn.exitonclick()
