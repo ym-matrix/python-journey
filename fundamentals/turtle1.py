d5fa2db00efba9ec659646c003e640685ca1f2b8
Yatharth Mehra <mehrayatharth07@gmail.com>
2026-08-31 22:31:52 +0530

    Added comments to turtle1 program

--- fundamentals/turtle1.py
+++ fundamentals/turtle1.py
@@ -1,12 +1,12 @@
-import turtle 
-wn = turtle.Screen()
-wn.bgcolor("black")
-alex=turtle.Turtle()
+import turtle                  #import the turtle graphics module
+wn = turtle.Screen()           #create a graphics window
+wn.bgcolor("black")            #set background color to black
+alex=turtle.Turtle()           #create a turtle named alex
 alex.color("orange")
-alex.pensize(5)
+alex.pensize(5)                #set the pen size to 5
 
-alex.forward(150)
-alex.left(90)   
+alex.forward(150)              #method to move the turtle forward by 150 units
+alex.left(90)                   #method to turn the turtle left by 90 degrees
 alex.forward(150)
 alex.left(45)
 alex.forward(105)
