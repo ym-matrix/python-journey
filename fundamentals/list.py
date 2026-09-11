7a3cdd2fcf1d0cacac5c35949005ab9895555c4e
Yatharth Mehra <mehrayatharth07@gmail.com>
2026-09-11 20:20:46 +0530

    Added list basic program

---
+++ fundamentals/list.py
@@ -0,0 +1,17 @@
+# A list can contain different data types.
+list = ["hello", 123, "123", 2.30]
+
+# Print the number of items in the list.
+print(len(list))
+
+# Print elements from index 0 up to index 1.
+print(list[0:2])
+
+# Print the item at index 2.
+print(list[2])
+
+# Concatenate the list with another list.
+print(list + [7, 8, 9])
+
+# Count how many times 123 appears in the list.
+print(list.count(123))
