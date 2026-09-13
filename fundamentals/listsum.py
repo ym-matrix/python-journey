e3f47bfdae72a3fb6ef0ef4425c9700f90efc219
Yatharth Mehra <mehrayatharth07@gmail.com>
2026-09-13 13:26:38 +0530

    Made changes to list sum program

--- fundamentals/listsum.py
+++ fundamentals/listsum.py
@@ -1,8 +1,10 @@
 user_input=(input("Enter numbers seperated by spaces:"))
+#to split the numbers at the trailing spaces
 number=[int(num) for num in user_input.split()]
 even_sum=0
 
 for num in number:
+# condition to check even 
     if num%2==0:
      even_sum+=num
 
