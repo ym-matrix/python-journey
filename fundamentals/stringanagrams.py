9338bde0cc51cd77fc6d2414df6d67f0eeec4aed
Yatharth Mehra <mehrayatharth07@gmail.com>
2026-09-13 14:02:14 +0530

    Added comments

--- fundamentals/stringanagrams.py
+++ fundamentals/stringanagrams.py
@@ -1,9 +1,12 @@
+# Ask the user to enter two words
 word1 = input("Enter a word:")
 word2 = input("Enter another word:")
 
+# Convert both words to lowercase and sort their letters
 cleaned_word1 = sorted(word1.lower())
 cleaned_word2 = sorted(word2.lower())
 
+# Compare the sorted letters to check if they are anagrams
 if cleaned_word1 == cleaned_word2:
     print(f"'{word1}' and '{word2}' are anagrams!")
 else:
